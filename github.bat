ead7a3886f2ebd349ef2627ec301a30007f86921
gopiprasanth <gopi.prasanth007@gmail.com>
2019-06-10 14:58:53 +0530

    updated git scm collector.

--- github.bat
+++ github.bat
@@ -1,2 +1,2 @@
 cd collectors\github
-java -jar github-scm-collector-3.0.1.jar --spring.config.name=github --spring.config.location=.\github.properties
+java -jar github-scm-collector.jar --spring.config.name=github --spring.config.location=.\github.properties
